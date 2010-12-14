559f01c1030415f514ca46849ba4594ff3c06e9a
Eric Meyer <emeyer@8thlight.com>
2010-12-14 14:07:02 -0600

    working solution

--- spec/core_spec.clj
+++ spec/core_spec.clj
@@ -1,12 +1,42 @@
 (ns basics-spec
-  (:use [speclj.core]))
+  (:use [speclj.core]
+        [euler2.core])
+  (:require [euler2.core]))
 
-(describe "Truth"
+(describe "Euler 2 - sum even numbers of the fibonacci sequence up to a max"
 
-  (it "is true"
-    (should true))
+  (it "sums up to 1" ;; 1
+    (should= 0 (euler2 1)))
+  
+  (it "sums up to 2" ;; 1 2
+    (should= 2 (euler2 2)))
 
-  (it "is not false"
-    (should-not false)))
+  (it "sums up to 3" ;; 1 2 3
+    (should= 2 (euler2 3)))
+  
+  (it "sums up to 5" ;; 1 2 3 5
+    (should= 2 (euler2 5)))
+
+  (it "sums up to 8" ;; 1 2 3 5 8
+    (should= 10 (euler2 8)))
+
+  (it "sums up to 8" ;; 1 2 3 5 8
+    (should= 4613732 (euler2 4000000)))
+        
+)
+
+(describe "fib-seq"
+  (it "gets up to 1"
+    (should= [1] (fib-seq-up-to 1)))
+
+  (it "gets up to 2"
+    (should= [1 2] (fib-seq-up-to 2)))
+
+  (it "gets up to 3"
+    (should= [1 2 3] (fib-seq-up-to 3)))
+
+  (it "gets up to 5"
+    (should= [1 2 3 5] (fib-seq-up-to 5)))
+)
 
 (run-specs)
--- src/euler2/core.clj
+++ src/euler2/core.clj
@@ -1,1 +1,19 @@
 (ns euler2.core)
+
+(defn second-to-last [list]
+  (last (pop list)))
+  
+(defn fib [list max]
+  (if (> (+ (last list) (second-to-last list)) max)
+    list
+    (fib (vec (concat list [(+ (last list) (second-to-last list))])) max)))
+
+(defn fib-seq-up-to [max]
+  (if (= max 1)
+    [1]
+    (if (= max 2)
+      [1 2]
+      (fib [1 2] max))))
+
+(defn euler2 [max]
+  (reduce + (filter even? (fib-seq-up-to max))))
